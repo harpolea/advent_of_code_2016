46328897213d3efa933062e8543c17d949fa2e8d
Alice Harpole <alice.harpole@soton.ac.uk>
2016-12-05 11:22:28 +0000

    Part 2 of day 4

--- day4.py
+++ day4.py
@@ -33,12 +33,37 @@ def find_rooms_part_1(in_file):
                 test_checksum += ''.join(l)
                 i = j
 
-        print(letters, commonest, sec_id, checksum)
-        print('Found checksum to be: {}'.format(test_checksum[0:5]))
+        #print(letters, commonest, sec_id, checksum)
+        #print('Found checksum to be: {}'.format(test_checksum[0:5]))
         if test_checksum[0:5] == checksum:
             total_sum += sec_id
 
     print("sum of valid room sec_ids: {}".format(total_sum))
 
+def find_rooms_part_2(in_file):
+    # read file
+    f = open(in_file, 'r')
+    total_sum = 0
+    for line in f:
+        letters = ''
+        checksum = line[-7:-2]
+        sec_id = int(line[-11:-8])
+        # calculate how much to shift by
+        shift = sec_id % 26
+
+        for l in line:
+            if l.isalpha():
+                l = chr((ord(l) - ord('a') + shift) % 26 + ord('a'))
+                letters += l
+            if l == '-':
+                letters += ' '
+            elif l == '[':
+                break
+
+        print(letters)
+
+        if 'object' in letters:
+            print("\n\nNorth pole objects at {}\n\n".format(sec_id))
+
 if __name__ == "__main__":
-    find_rooms_part_1('day4_input.txt')
+    find_rooms_part_2('day4_input.txt')
