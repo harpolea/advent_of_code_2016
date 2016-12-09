8405858475f23acf3b6238e68917128a220ebabd
Alice Harpole <alice.harpole@soton.ac.uk>
2016-12-09 11:37:24 +0000

    Part 2 solution

--- day9.py
+++ day9.py
@@ -1,6 +1,18 @@
 import numpy as np
 import re
 
+def decompressed_len(t):
+    print(t)
+    m = re.search('\((\d+)x(\d+)\)', t)
+    if m is None:
+        print(t, len(t))
+        return len(t)
+    else:
+        i = int(m.group(1))
+        j = int(m.group(2))
+
+        return len(t[:t.index('(')]) + j * decompressed_len(t[t.index(')')+1:t.index(')')+1+i]) + decompressed_len(t[t.index(')')+1+i:])
+
 def decompress(in_file):
     # read file
     f = open(in_file, 'r')
@@ -8,7 +20,6 @@ def decompress(in_file):
     text = f.read()
     text = re.sub('[ \n]', '', text)
     counter = 0
-
     decompressed_file_len = 0
 
     while counter < len(text):
@@ -21,7 +32,6 @@ def decompress(in_file):
             i = int(i)
             j = int(j)
             data = text[counter:counter+i]
-            print(data, '\n')
             decompressed_file_len += j * len(data)
             counter += i
         else:
@@ -29,6 +39,8 @@ def decompress(in_file):
             counter += 1
 
     print('Length of decompressed file is {}'.format(decompressed_file_len))
+
+    print('Length of decompressed file in format 2 is {}'.format(decompressed_len(text)))
 
 
 if __name__ == "__main__":
