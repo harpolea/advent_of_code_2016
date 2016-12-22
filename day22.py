7d6d300319c5bf57f69529139b47a50be8c83fdb
Alice Harpole <ah1e14@soton.ac.uk>
2016-12-22 11:06:27 +0000

    Pretty printing of nodes in day 22

--- day22.py
+++ day22.py
@@ -16,6 +16,9 @@ def nodes(in_file):
     ls = f.readlines()
     nodes = []
 
+    max_x = 0
+    max_y = 0
+
     for l in ls:
         m = re.match('/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T\s+(\d+)%', l)
         if m is not None:
@@ -27,6 +30,8 @@ def nodes(in_file):
             use = int(m.group(1))
 
             nodes.append(Node(x, y, size, used, avail, use))
+            if x > max_x: max_x = x
+            if y > max_y: max_y = y
 
     n_viable = 0
 
@@ -35,6 +40,21 @@ def nodes(in_file):
             if (n.used > 0 and n.used < m.avail) or (m.used > 0 and m.used < n.avail):
                 n_viable += 1
     print('There are {} viable nodes.'.format(n_viable))
+
+    # put node data into grid
+    grid = [['.' for i in range(max_x+1)] for j in range(max_y+1)] # index [y][x]
+    for n in nodes:
+        if n.used / n.size > 0.9:
+            grid[n.y][n.x] = '#'
+        elif n.used == 0:
+            grid[n.y][n.x] = '_'
+        if n.y == 0 and n.x == max_x:
+            grid[n.y][n.x] = 'G'
+
+    grid[0][0] = 'x'
+
+    for r in grid:
+        print(' '.join(r))
 
     return
 
